65017844713761c123ea1835f71401a0a63e432f
Azecko <samikarin24@gmail.com>
2025-09-16 09:14:13 +0200

    [feature] `/api/exams` route
    API route using the `getAllExams` function to fetch all the exams in the database and return it as JSON.

---
+++ app/lib/database.ts
@@ -0,0 +1,20 @@
+import mysql from 'mysql2';
+
+export function getAllExams() {
+    const connection = mysql.createConnection({
+        host: process.env.MYSQL_HOST,
+        user: process.env.MYSQL_USER,
+        password: process.env.MYSQL_PASSWORD,
+        database: process.env.MYSQL_DATABASE,
+    })
+
+    connection.connect()
+
+    return new Promise(function(resolve, reject) {
+        connection.query('SELECT * from exam;', (err, rows, fields) => {
+            if (err) throw err
+            resolve(rows);
+        })
+        connection.end()
+    })
+}
