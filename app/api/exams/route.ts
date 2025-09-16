a5f591b9573232331b316e63a98e5e147dd1d2d8
Azecko <30987143+Azecko@users.noreply.github.com>
2025-09-16 10:30:09 +0200

    Use the database for calendar (#16)
    * [feature] `/api/exams` route
    
    API route using the `getAllExams` function to fetch all the exams in the database and return it as JSON.
    
    * [feature] Use exams in calendar
    
    Using the previously created `/api/exams` route, the calendar now uses the exams present in the database.
    
    * [feature] .env.example
    
    * [fix] Wrong table name...
    
    Oops... The right table name to use is `crep`.

---
+++ app/api/exams/route.ts
@@ -0,0 +1,12 @@
+// TODO: Authenticate / Authorize this route to avoid anybody retreiving data.
+
+import { getAllExams } from "@/app/lib/database";
+
+export async function GET() {
+    const exams = await getAllExams();
+
+    return new Response(JSON.stringify(exams), {
+        status: 200,
+        headers: { 'Content-Type': 'application/json' },
+    });
+}
