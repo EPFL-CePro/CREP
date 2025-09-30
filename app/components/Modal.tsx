7dc9d2ea2cdacda8ccc6865e4f0e16831cedea47
alessdangelo <61836900+alessdangelo@users.noreply.github.com>
2025-09-30 10:11:02 +0200

    style: remove dark mode

--- app/components/Modal.tsx
+++ app/components/Modal.tsx
@@ -58,7 +58,7 @@ export function Modal({ event, shareLink, user, examStatus, exams, setExams, cal
     return (
         <form method="dialog" className="modal-content flex flex-col gap-4 p-12 w-full text-foreground bg-background accent-red-500 [&_input]:rounded-lg">
             <h3 className={`font-bold basis-full text-lg ${examColor}`}>{event?.title}</h3>
-            <button className=" btn p-1 absolute right-5 top-5 hover:bg-gray-100 dark:hover:bg-stone-800" aria-label="Close">
+            <button className=" btn p-1 absolute right-5 top-5 hover:bg-gray-100" aria-label="Close">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="h-6 w-6 size-6 ">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12" />
                 </svg>
