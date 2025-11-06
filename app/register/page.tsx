05d6f6a9b2a375fd6be92e6f0ed3f69e2eb231cf
alessdangelo <61836900+alessdangelo@users.noreply.github.com>
2025-11-06 09:28:33 +0100

    feat: add exam register page

---
+++ app/register/page.tsx
@@ -0,0 +1,13 @@
+import RegisterForm from "../components/forms/register"
+
+export const metadata = {
+    title: "Register your exam",
+}
+
+export default function Page() {
+    return (
+        <main>
+            <RegisterForm />
+        </main>
+    )
+}
