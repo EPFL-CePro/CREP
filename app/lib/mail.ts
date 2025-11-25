8229ba1fc9c34d31c2550b5ddaa906f8563700e4
Azecko <samikarin24@gmail.com>
2025-11-25 09:32:34 +0100

    [feature] Send email on printing subscription
    Email is actually sent to the cepro exams's email adress, will change in the future when the authentication on the `register` page is made.
    The email content is subject to change also.

---
+++ app/lib/mail.ts
@@ -0,0 +1,22 @@
+'use server';
+
+import nodemailer from 'nodemailer';
+
+export async function sendMail(to: string, subject: string, content: string) {
+    const transporter = nodemailer.createTransport({
+        host: process.env.MAIL_SMTP_HOST,
+        port: 587,
+        secure: false,
+        auth: {
+            user: process.env.MAIL_AUTH_USER,
+            pass: process.env.MAIL_AUTH_PASS
+        }
+    });
+
+    return transporter.sendMail({
+        from: process.env.MAIL_FROM_EMAIL,
+        to: to,
+        subject: subject,
+        text: content
+    });
+}
