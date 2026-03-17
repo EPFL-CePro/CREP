1c811c1a830404c519f76486a443708695783532
Azecko <samikarin24@gmail.com>
2026-03-17 15:22:52 +0100

    [refactor] Type in a declared in a function...
    The type of the parameter required for the `insertExam` function was declared directly in the function which is a bit dirty and makes it hard to re-use the type.
    It has now been moved inside a type named `Exam` , in a dedicated file.

---
+++ types/exam.ts
@@ -0,0 +1,20 @@
+export type Exam = {
+    code: string;
+    name: string;
+    service_level_id: number;
+    service_id: number;
+    exam_type_id: number;
+    exam_status_id: number;
+    exam_date?: string | Date | null;
+    academic_year_id: number;
+    exam_semester: number;
+    nb_students?: number | null;
+    nb_pages?: number | null;
+    total_pages?: number | null;
+    // deadline_prep: string | Date;
+    // deadline_repro: string | Date;
+    remark?: string | null;
+    section_id: number;
+    responsible_id: number | null;
+    contact: string;
+}
