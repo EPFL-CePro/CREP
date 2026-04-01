3a831370041b6c90ef55e9d32745a922136b296a
Azecko <samikarin24@gmail.com>
2026-04-01 10:58:25 +0200

    [feature] Changing exam type in the UI updates the database

--- app/lib/database.ts
+++ app/lib/database.ts
@@ -575,3 +575,22 @@ export async function updateExamService(examId: string, serviceId: string) {
         connection.end()
     })
 }
+
+export async function updateExamType(examId: string, examTypeId: string) {
+    const connection = mysql.createConnection({
+        host: process.env.MYSQL_HOST,
+        user: process.env.MYSQL_USER,
+        password: process.env.MYSQL_PASSWORD,
+        database: process.env.MYSQL_DATABASE,
+    })
+
+    connection.connect()
+
+    return new Promise(function(resolve) {
+        connection.query('UPDATE exam SET exam_type_id = ? WHERE id = ?;', [examTypeId, examId], (err, rows) => {
+            if (err) throw err
+            resolve(JSON.stringify(rows));
+        })
+        connection.end()
+    })
+}
