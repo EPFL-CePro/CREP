08832ecc8c0a8d759afe182beba901c19dc859e7
Azecko <samikarin24@gmail.com>
2026-05-05 10:25:29 +0200

    [refactor] Removed `Description` from the Modal
    Probably useless, will see in the future.

--- app/components/Modal.tsx
+++ app/components/Modal.tsx
@@ -188,10 +188,6 @@ export function Modal({ event, user, examStatus, exams, setExams }: ModalProps) 
                             </div>
                         </div>
                     </div>
-                    <div>
-                        <label className="font-semibold w-full" htmlFor="description">Description</label>
-                        <p className="description">{event?.extendedProps?.description}</p>
-                    </div>
                     <textarea className="remarks min-h-32 resize-y rounded-lg border border-gray-300 p-3" rows={6} name="remarks" id="remarks" placeholder="Add any remarks"
                         value={remark || ""}
                         onChange={(e) => setRemark(e.target.value)}
